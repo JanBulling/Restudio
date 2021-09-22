a1816c719cccf4f7bf4107dcce076197f54bff4d
Janbulling <janbulling3141@gmail.com>
2021-09-22 16:10:33 +0200

    Firebase and dependencies added

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+  runApp(RestudioApp());
 }
 
-class MyApp extends StatelessWidget {
+class RestudioApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
