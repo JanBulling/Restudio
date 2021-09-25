4b6e0c578ef52e93427a1cdd05dacb04f3af9767
Janbulling <janbulling3141@gmail.com>
2021-09-25 11:55:04 +0200

    custom buttons, welcome screen, login screen

--- lib/config/theme.dart
+++ lib/config/theme.dart
@@ -6,51 +6,66 @@ ThemeData theme() {
     primaryColorDark: Color(0xFF890033),
     primaryColorLight: Color(0xFFE75A8E),
     scaffoldBackgroundColor: Colors.white,
+    errorColor: Color(0xFF730202),
+    hoverColor: Color(0xFFA290CE),
     fontFamily: "Poppins",
     backgroundColor: Color(0xFFF5F5F5),
     textTheme: TextTheme(
       headline1: TextStyle(
-        color: Color(0xFF1B070B),
+        color: black,
         fontWeight: FontWeight.bold,
         fontSize: 36,
       ),
       headline2: TextStyle(
-        color: Color(0xFF1B070B),
+        color: black,
         fontWeight: FontWeight.bold,
         fontSize: 24,
       ),
       headline3: TextStyle(
-        color: Color(0xFF1B070B),
+        color: black,
         fontWeight: FontWeight.bold,
         fontSize: 18,
       ),
       headline4: TextStyle(
-        color: Color(0xFF1B070B),
+        color: black,
         fontWeight: FontWeight.bold,
         fontSize: 16,
       ),
       headline5: TextStyle(
-        color: Color(0xFF1B070B),
+        color: black,
         fontWeight: FontWeight.bold,
         fontSize: 14,
       ),
       headline6: TextStyle(
-        color: Color(0xFF1B070B),
+        color: black,
         fontWeight: FontWeight.normal,
         fontSize: 14,
       ),
       bodyText1: TextStyle(
-        color: Color(0xFF1B070B),
+        color: black,
         fontWeight: FontWeight.normal,
         fontSize: 12,
       ),
       bodyText2: TextStyle(
-        color: Color(0xFF1B070B),
+        color: black,
         fontWeight: FontWeight.normal,
         fontSize: 10,
+      ),
+      button: TextStyle(
+        fontFamily: "Roboto",
       ),
     ),
   );
 }
 
+const Color black = Color(0xFF1B070B);
 const double padding = 16.0;
+
+InputDecoration inputDecoration({required String label, IconData? icon}) => InputDecoration(
+      labelText: label,
+      filled: true,
+      fillColor: Colors.grey.shade100,
+      border: OutlineInputBorder(borderSide: BorderSide.none),
+      labelStyle: TextStyle(color: Colors.grey.shade600),
+      prefixIcon: Icon(icon, color: Colors.grey.shade600),
+    );
